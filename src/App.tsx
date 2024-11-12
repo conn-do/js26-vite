6753d659f9828e080654905e2b5e34a34eebcf27
conn-do <kondo@japaan.jp>
2024-11-12 11:02:06 +0900

    2024-11-12 の授業内容
    TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,72 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+type Name = {
+  name: string
+}
+type Age = {
+  age: number
+}
+
+type Person = Name &
+  Age & {
+    isStudent?: boolean
+  }
+
+function sample() {
+  let sampleText: string = 'sample'
+  sampleText = 100
+  let sampleBoolean: boolean = false
+  sampleBoolean = 'true'
+  const sampleArray: number[] = [1, 2, '3']
+  console.log(sampleText)
+  console.log(sampleBoolean)
+  console.log(sampleArray)
+
+  // ? を付けると任意のプロパティになる
+  const sampleObject: Person = {
+    name: 'こんどう',
+    age: 31,
+  }
+  sampleObject.isStudent = true
+
+  console.log(sampleObject)
+}
+
+function sample2({
+  a,
+  b,
+  twice: c,
+  ...rest
+}: {
+  a: string
+  b: number
+  twice: (d: number) => number
+  hoge: string
+  fuga: number
+}): number {
+  console.log(a + b.toString())
+  console.log(c(20))
+  console.log(rest)
+  return Number.parseInt(a) + b
+}
+
 function App() {
   const [count, setCount] = useState(0)
   const name = 'こんどうです'
+  sample()
+  function twice(d: number): number {
+    return d * 2
+  }
+  const a = '10'
+  const result = sample2({
+    a,
+    b: 20,
+    twice,
+    hoge: 'hoge',
+    fuga: 100,
+  })
+  console.log(result)
 
   return (
     <>
