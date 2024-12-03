ec53329b77889527c48739e4879d3d57fe173973
conn-do <kondo@japaan.jp>
2024-12-03 11:10:11 +0900

    2024-12-03 の授業 Teams を作る

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Form from './Form.tsx'
+import Teams from './Teams.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Form />
+    <Teams />
   </StrictMode>
 )
